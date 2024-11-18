4a2366bf8cd9608ec72a9135b1c78df38b529dee
Byeonghun <qudgns5142@gmail.com>
2024-11-18 19:28:59 +0900

    feat: 로딩 스피너 데이터가 수신되는데 오래 걸릴 때 로딩 스피너 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef } from 'react';
 import './App.css';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
 import Button from 'react-bootstrap/Button';
 import Collapse from 'react-bootstrap/Collapse';
 import Card from 'react-bootstrap/Card';
@@ -14,6 +15,7 @@ function App() {
   let [selectedTopic, setSelectedTopic] = useState(null);
   let [openStates, setOpenStates] = useState({});
   let [itemsToShow, setItemsToShow] = useState(10);
+  let [loading, setLoading] = useState(false);
   let navigate = useNavigate();
   const loader = useRef(null);
 
@@ -45,6 +47,7 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const response = await axios.get('http://springboot-developer-env.eba-zqkfw5p2.ap-northeast-2.elasticbeanstalk.com/');
         console.log('성공', response.data);
@@ -53,6 +56,7 @@ function App() {
       catch (error) {
         console.error('실패?', error);
       }
+      setLoading(false);
     };
     fetchData();
   }, []);
@@ -90,43 +94,49 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Routes>
-        <Route path="/" element={
-          <div>
-            <Container>
-              <Row xs={1} md={2} className="g-4">
-                {currentData.map((item, index) => (
-                  <Col key={index}>
-                    <Card>
-                      <Card.Img variant="top" src={item.image} style={{ width: '100px', height: '100px', objectFit: 'cover', margin: '0 auto' }} />
-                      <Card.Body>
-                        <Card.Title onClick={() => { navigate(`/detail/${item.id}`) }} style={{ cursor: 'pointer' }}>{item.title}</Card.Title>
-                        <Card.Text>
-                          {item.content}
-                          <br />
-                          <Button onClick={() => handleToggle(item.id)}
-                            aria-controls={`example-collapse-text-${item.id}`}
-                            aria-expanded={openStates[item.id]}>
-                            의견보기
-                          </Button>
-                          <Collapse in={openStates[item.id]}>
-                            <div id={`example-collapse-text-${item.id}`}>
-                              {item.opinion}
-                            </div>
-                          </Collapse>
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                ))}
-              </Row>
-              <div ref={loader} />
-            </Container>
-          </div>
-        } />
-        <Route path="/detail/:id" element={<Detail newsdata={newsdata} />} />
-        <Route path="*" element={<h1>Not Found</h1>} />
-      </Routes>
+      {loading ? (
+        <Container className="d-flex justify-content-center my-5">
+          <Spinner animation="border" role="status"></Spinner>
+        </Container>
+      ) : (
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <Container>
+                <Row xs={1} md={2} className="g-4">
+                  {currentData.map((item, index) => (
+                    <Col key={index}>
+                      <Card>
+                        <Card.Img variant="top" src={item.image} style={{ width: '100px', height: '100px', objectFit: 'cover', margin: '0 auto' }} />
+                        <Card.Body>
+                          <Card.Title onClick={() => { navigate(`/detail/${item.id}`) }} style={{ cursor: 'pointer' }}>{item.title}</Card.Title>
+                          <Card.Text>
+                            {item.content}
+                            <br />
+                            <Button onClick={() => handleToggle(item.id)}
+                              aria-controls={`example-collapse-text-${item.id}`}
+                              aria-expanded={openStates[item.id]}>
+                              의견보기
+                            </Button>
+                            <Collapse in={openStates[item.id]}>
+                              <div id={`example-collapse-text-${item.id}`}>
+                                {item.opinion}
+                              </div>
+                            </Collapse>
+                          </Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  ))}
+                </Row>
+                <div ref={loader} />
+              </Container>
+            </div>
+          } />
+          <Route path="/detail/:id" element={<Detail newsdata={newsdata} />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      )}
     </div>
   );
 }
