a032d0f9395c5816369dcc199538956e376c2359
YangByeongHun <qudgns5142@gmail.com>
2024-12-11 18:07:42 +0900

    feat: NewsCard 컴포넌트 수정 Card크기 고정, 130자 이상시 펼쳐보기 기능 추가

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,19 +1,31 @@
 // NewsCard.js
-import React from 'react';
+import { React, useState } from 'react';
 import { Card, Button, Collapse, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
+
 function NewsCard({ item, openStates, handleToggle }) {
+  const [isExpanded, setIsExpanded] = useState(false);
   let navigate = useNavigate();
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   return (
     <Col>
-      <Card className="shadow-sm mb-4">
+      <Card className="shadow-sm mb-4" style={{ width: '600px', height: isExpanded ? 'auto' : '310px', overflow: 'hidden' }}>
         <Card.Img variant="top" src={item.image} style={{ width: '100px', height: '100px', objectFit: 'cover', margin: '0 auto' }} />
         <Card.Body>
           <Card.Title onClick={() => { navigate(`/detail/${item.id}`) }} style={{ cursor: 'pointer' }}>{item.title}</Card.Title>
-          <Card.Text>
-            {item.content}
+          <Card.Text style={{ maxHeight: isExpanded ? 'none' : '200px', overflow: isExpanded ? 'visible' : 'hidden' }}>
+            {isExpanded || item.content.length <= 150 ? item.content : `${item.content.substring(0, 130)}...`}
+            <br />
+            {item.content.length > 130 && (
+              <Button variant="link" onClick={toggleExpand}>
+                {isExpanded ? '접기' : '펼쳐보기'}
+              </Button>
+            )}
             <br />
             <Button onClick={() => handleToggle(item.id)}
               aria-controls={`example-collapse-text-${item.id}`}
