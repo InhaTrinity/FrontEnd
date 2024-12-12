a473f6af02a5d8b8832599b18a07dfe169e77117
YangByeongHun <qudgns5142@gmail.com>
2024-12-12 21:05:29 +0900

    feat: 버튼 반응형 레이아웃

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,6 +1,6 @@
 // SearchBar.js
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Col, Row } from 'react-bootstrap';
 
 function SearchBar({ searchInput, setSearchInput, handleSearch, darkMode, handleDarkModeToggle }) {
   const handleSubmit = (e) => {
@@ -9,19 +9,27 @@ function SearchBar({ searchInput, setSearchInput, handleSearch, darkMode, handle
   };
 
   return (
-    <Form className="d-flex" onSubmit={handleSubmit}>
-      <Form.Group controlId="search" className="flex-grow-1">
-        <Form.Control
-          type="text"
-          placeholder="검색어를 입력하세요"
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit" className="ms-2">검색하기</Button>
-      <Button variant={darkMode ? "light" : "dark"} onClick={handleDarkModeToggle} className="ms-2">
-        {darkMode ? "라이트 모드" : "다크 모드"}
-      </Button>
+    <Form onSubmit={handleSubmit}>
+      <Row className="g-2">
+        <Col xs={12} md={8}>
+          <Form.Group controlId="search">
+            <Form.Control
+              type="text"
+              placeholder="검색어를 입력하세요"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
+        <Col xs={6} md={2}>
+          <Button variant="primary" type="submit" className="w-100">검색하기</Button>
+        </Col>
+        <Col xs={6} md={2}>
+          <Button variant={darkMode ? "light" : "dark"} onClick={handleDarkModeToggle} className="w-100">
+            {darkMode ? "라이트 모드" : "다크 모드"}
+          </Button>
+        </Col>
+      </Row>
     </Form>
   );
 }
