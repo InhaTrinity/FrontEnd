682403c0d94011dcaaf5ae68fcc362e9140b522f
YangByeongHun <qudgns5142@gmail.com>
2024-12-12 20:36:27 +0900

    chore: 오타수정

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,14 +1,34 @@
 //NewsCard.js
+import { useEffect } from 'react';
 import { React, useState } from 'react';
 import { Card, Button, Collapse, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 function NewsCard({ item, openStates, handleToggle }) {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [isBookmarked, setIsBookmarked] = useState(false);
   let navigate = useNavigate();
+
+  useEffect(() => {
+    const bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+    setIsBookmarked(bookmarks.some(bookmark => bookmark.id === item.id));
+  }, [item.id]);
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
+  };
+
+  const handleBookmark = () => {
+    const bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+    if (isBookmarked) {
+      const newBookmarks = bookmarks.filter(bookmark => bookmark.id !== item.id);
+      localStorage.setItem('bookmarks', JSON.stringify(newBookmarks));
+      setIsBookmarked(false);
+    } else {
+      bookmarks.push(item);
+      localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+      setIsBookmarked(true);
+    }
   };
 
   return (
@@ -20,7 +40,7 @@ function NewsCard({ item, openStates, handleToggle }) {
         <Card.Body className="d-flex flex-column justify-content-center align-items-center text-center">
           <Card.Title onClick={() => { navigate(`/detail/${item.id}`) }} style={{ cursor: 'pointer' }}>{item.title}</Card.Title>
           <Card.Text>
-            {isExpanded || item.content.length <= 150 ? item.content : `${item.content.substring(0, 130)}...`}
+            {isExpanded || item.content.length <= 130 ? item.content : `${item.content.substring(0, 130)}...`}
             <br />
             {item.content.length > 130 && (
               <Button variant="link" onClick={toggleExpand}>
@@ -38,6 +58,10 @@ function NewsCard({ item, openStates, handleToggle }) {
                 {item.opinion}
               </div>
             </Collapse>
+            <br />
+            <Button variant = {isBookmarked ? "danger" : "outline-danger"} onClick={handleBookmark}>
+              {isBookmarked ? '북마크 해제' : '북마크'}
+            </Button>
           </Card.Text>
         </Card.Body>
       </Card>
