9c3a3df52965dbb73bd68b601aff425b54dd27d6
Byeonghun <qudgns5142@gmail.com>
2024-12-15 03:04:16 +0900

    chore: title, content 구분선 추가

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -84,7 +84,7 @@ function Detail({ newsdata, darkMode }) {
                     <Card.Title>{item.title}</Card.Title>
                     <Card.Text>
                         {item.content}
-                        <br></br>
+                        <div style={{ width: '100%', height: '1px', backgroundColor: '#ccc', margin: '10px 0' }}></div>
                         {item.opinion}
                     </Card.Text>
                     <Button variant="secondary" onClick={() => navigate(-1)} className="me-2">
