22fd0a0640ee8c6fc9328321f19e58788d80f6dc
Byeonghun <qudgns5142@gmail.com>
2024-12-17 08:30:13 +0900

    feat: opinion 삭제제

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -96,8 +96,6 @@ function Detail({ newsdata, darkMode }) {
                     <Card.Title>{item.title}</Card.Title>
                     <Card.Text>
                         {item.content}
-                        <div style={{ width: '100%', height: '1px', backgroundColor: '#ccc', margin: '10px 0' }}></div>
-                        {item.opinion}
                     </Card.Text>
                     <Button variant="secondary" onClick={() => navigate(-1)} className="me-2">
                         <FontAwesomeIcon icon={faArrowLeft} /> 뒤로 가기
