6f0e52e2719bb7190e9d963ae74f20ea3a157568
YangByeongHun <qudgns5142@gmail.com>
2024-12-17 00:20:10 +0900

    feat: 라우터추가 Navbar에 라우터를 추가해 detail페이지에서도 이동가능하게 만듦

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 // NavBar.js
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function NavBar({ topics, handleTopicClick, handleMainClick }) {
+  const navigate = useNavigate();
+
+  const handleTopic = (topic) => {
+    handleTopicClick(topic); // 선택된 주제를 상위 컴포넌트로 전달
+    navigate(`/topic/${encodeURIComponent(topic)}`); // 해당 주제로 라우팅
+  };
+
   return (
     <Navbar collapseOnSelect expand="lg" data-bs-theme="dark" bg="dark" className="bg-body-tertiary">
       <Container>
@@ -13,7 +20,7 @@ function NavBar({ topics, handleTopicClick, handleMainClick }) {
           <Nav className="ms-auto">
             <Nav.Link onClick={handleMainClick}>전체보기</Nav.Link>
             {topics.map((topic, index) => (
-              <Nav.Link key={index} onClick={() => handleTopicClick(topic)}>{topic}</Nav.Link>
+              <Nav.Link key={index} onClick={() => handleTopic(topic)}>{topic}</Nav.Link>
             ))}
             <Nav.Link as={Link} to="/bookmarks">북마크</Nav.Link>
           </Nav>
